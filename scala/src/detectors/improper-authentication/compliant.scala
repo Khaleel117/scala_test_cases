ef30017de012991570ce735b88fe6667aa496a12
Khaleel117 <khtsayed@amazon.com>
2025-09-25 17:40:19 +0530

    Update compliant.scala

--- scala/src/detectors/improper-authentication/compliant.scala
+++ scala/src/detectors/improper-authentication/compliant.scala
@@ -9,7 +9,7 @@ class ImproperAuthenticationCompliant{
 
     // {fact rule=improper-authentication@v1.0 defects=0}
     def compliant(): BasicParserPool = {
-        val parserPool = new BasicParserPool
+        val parserPool = new BasicParserPool 
         // Compliant: Defining a method to create a parser pool with ignoreComments set to true.
         parserPool.setIgnoreComments(true)
         parserPool
