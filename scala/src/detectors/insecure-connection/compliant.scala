69a2a517e7db36e7aa60668dd3b31999645800c7
Khaleel117 <khtsayed@amazon.com>
2025-09-25 17:43:29 +0530

    Update compliant.scala

--- scala/src/detectors/insecure-connection/compliant.scala
+++ scala/src/detectors/insecure-connection/compliant.scala
@@ -9,7 +9,7 @@ object InsecureConnectionCompliant {
     // {fact rule=insecure-connection@v1.0 defects=0}
     @throws[IOException]
     private[this] def compliant(): Unit = {
-        // Compliant: The connection is secure.
+        // Compliant: The connection is secure. 
         val soc = SSLSocketFactory.getDefault.createSocket("www.google.com", 443)
         doGetRequest(soc)
     }
