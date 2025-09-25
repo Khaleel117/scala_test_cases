829ee37db06031a55a93d52298ac0145550328a3
Khaleel117 <khtsayed@amazon.com>
2025-09-25 17:38:21 +0530

    Update compliant.scala

--- scala/src/detectors/deserialization-of-untrusted-data/compliant.scala
+++ scala/src/detectors/deserialization-of-untrusted-data/compliant.scala
@@ -8,7 +8,7 @@ import org.apache.xmlrpc.client.XmlRpcClientConfigImpl
 object DeserializationOfUntrustedDataCompliant {
     // {fact rule=deserialization-of-untrusted-data@v1.0 defects=0}
     def compliant(): Unit = {
-        val clientCfg = new XmlRpcClientConfigImpl
+        val clientCfg = new XmlRpcClientConfigImpl 
         // Compliant: Disabled extensions within an Apache XML-RPC server.
         clientCfg.setEnabledForExtensions(false)
     }
